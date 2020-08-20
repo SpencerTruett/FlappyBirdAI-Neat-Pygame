57607704df03897ee92956a3952fa0bb57b36126
Spencer Truett <srtruett1993@gmail.com>
2020-08-20 13:18:39 -0500

    added Draw method

--- FlappyBirdAI-NeatAndPygame.py
+++ FlappyBirdAI-NeatAndPygame.py
@@ -49,5 +49,31 @@ class Bird:
         if self.tilt < self.MAX_ROTATION:
           self.tilt = self.MAX_ROTATION
       else:
-        if self.tilt > -90:
+        if self.tilt > -90: #Creates downward arc
           self.tilt -= self.ROT_VEL
+
+              
+    def draw(self, win):
+      self.img_count += 1 #How many tics we've shown the current image
+
+      #What image we should show based on current image count
+      if self.img_count < self.ANIMATION_TIME:
+        self.img = self.IMGS[0]
+      elif self.img_count < self.ANIMATION_TIME*2:
+        self.img = self.IMGS[1]
+      elif self.img_count < self.ANIMATION_TIME*3:
+        self.img = self.IMGS[2]
+      elif self.img_count < self.ANIMATION_TIME*4:
+        self.img = self.IMGS[1]
+      elif self.img_count < self.ANIMATION_TIME*4 + 1:
+        self.img = self.IMGS[0]
+        self.img_count = 0
+
+      #Wings don't flap when nosediving
+      if self.tilt <= -80:
+        self.img = self.IMGS[1]
+        self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topLeft = (self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topLeft)
